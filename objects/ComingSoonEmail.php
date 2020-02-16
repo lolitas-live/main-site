2faa7799b7fa21e427e8a5b6ac9179960608accc
Juan D'Achiardi <jddach@gmail.com>
2020-02-16 19:11:37 +0000

    add check for email and log results

--- objects/ComingSoonEmail.php
+++ objects/ComingSoonEmail.php
@@ -7,16 +7,25 @@ if (!isset($global['systemRootPath'])) {
 
 function saveData() {
   global $global;
+
   // Check if the form is submitted
   if (isset($_POST['submit'])) {
 
     // retrieve the form data by using the element's name attributes value as key
     $email = $_REQUEST['email'];
+
+    function checkEmail() {
+      $sql = "SELECT * FROM `coming_soon_email` WHERE email = ?;";
+      $res = sqlDAL::readSql($sql, "s", array(xss_esc($email)));
+      echo "<script>console.log('Response: " . $res . "' );</script>";
+    }
+
     $sql = "INSERT INTO `coming_soon_email` (`email`, `created`, `modified`) VALUES "
     . "(?, now(), now())";
-    echo "<script>console.log('SQL statement: " . $sql . "' );</script>";
     sqlDAL::writeSql($sql, "s", array(xss_esc($email)));
+
   }
+  checkEmail();
 }
 
 saveData();
