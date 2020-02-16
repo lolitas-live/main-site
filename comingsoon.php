4fec7f82837d4ff0c1fa59c26b8d2898cb67e7a9
Juan D'Achiardi <jddach@gmail.com>
2020-02-16 17:31:32 +0000

    testing nested if

--- comingsoon.php
+++ comingsoon.php
@@ -131,25 +131,23 @@ hr {
 
 </style>
 <body>
+<?php if (isset($_POST['form_submitted'])): ?>
 <?php
-  echo("<script>console.log('got here pre isset');</script>");
-  if (isset($_POST['form_submitted'])):
     echo("<script>console.log('got isset');</script>");
-  function saveData() {
-  // Check if the form is submitted
-  if ( isset( $_POST['submit'] ) ) {
-    // retrieve the form data by using the element's name attributes value as key
-    $email = $_REQUEST['email'];
-    echo("<script>console.log('got here saveData');</script>");
+    function saveData() {
+      // Check if the form is submitted
+      if ( isset( $_POST['submit'] ) ) {
+        // retrieve the form data by using the element's name attributes value as key
+        $email = $_REQUEST['email'];
+        echo("<script>console.log('got here saveData');</script>");
 
-  }
-
-  $sql = "INSERT INTO coming_soon_email (email, created, modified,) "
-        . " VALUES ($email, now(), now(), ";
-  sqlDAL::writeSql($sql);
-}
-saveData();
-echo("<script>console.log('got here end');</script>");
+      }
+      $sql = "INSERT INTO coming_soon_email (email, created, modified,) "
+            . " VALUES ($email, now(), now(), ";
+      sqlDAL::writeSql($sql);
+    }
+  saveData();
+  echo("<script>console.log('got here end');</script>"); 
 ?>
 <?php else: ?>
 <div class="bgimg">
