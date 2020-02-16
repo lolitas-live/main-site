653aab916c5d17791b063e9c65edd5b875fbd5e5
Juan D'Achiardi <jddach@gmail.com>
2020-02-16 02:59:48 +0000

    debugging

--- objects/ComingSoonEmail.php
+++ objects/ComingSoonEmail.php
@@ -6,14 +6,15 @@ class ComingSoonEmail {
 
   private $email;
 
-  // Check if the form is submitted
-  if ( isset( $_POST['submit'] ) ) {
-    // retrieve the form data by using the element's name attributes value as key
-    $email = $_REQUEST['email'];
+  function save() {
+    // Check if the form is submitted
+    if ( isset( $_POST['submit'] ) ) {
+      // retrieve the form data by using the element's name attributes value as key
+      $email = $_REQUEST['email'];
+    }
+
+    $sql = "INSERT INTO coming_soon_email (email, created, modified,) "
+          . " VALUES ($email, now(), now(), ";
+    sqlDAL::writeSql($sql));
   }
-
-  $sql = "INSERT INTO coming_soon_email (email, created, modified,) "
-        . " VALUES ($email, now(), now(), ";
-  sqlDAL::writeSql($sql));
-
 }
