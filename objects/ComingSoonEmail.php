e44ae43efdd5ce46d7acae10388ddf81418810de
Juan D'Achiardi <jddach@gmail.com>
2020-02-16 19:44:36 +0000

    if data is false log email already exists

--- objects/ComingSoonEmail.php
+++ objects/ComingSoonEmail.php
@@ -15,27 +15,18 @@ function saveData() {
     $email = $_REQUEST['email'];
 
     function checkEmail() {
-      $sql = "SELECT * FROM `coming_soon_email` WHERE email = ?;";
+      $sql = "SELECT * FROM `coming_soon_email` WHERE email = ?";
       $res = sqlDAL::readSql($sql, "s", array(xss_esc($email)));
       $data = sqlDAL::fetchAssoc($res);
       sqlDAL::close($res);
-      if ($res) {
-        if (!empty($data)) {
-            // return $data;
-            echo "<script>console.log('DATA: " . $data . "' );</script>";
-        } else {
-          echo "<script>console.log('DATA is empty, res: " . $res . "' );</script>";
-        }
+      if ($data != false) {
+        echo "<script>console.log('Email already exists: " . $res . "' );</script>";
       } else {
-          echo "<script>console.log('RES is empty' );</script>";
-        }
+        $sql = "INSERT INTO `coming_soon_email` (`email`, `created`, `modified`) VALUES "
+        . "(?, now(), now())";
+        sqlDAL::writeSql($sql, "s", array(xss_esc($email)));
       }
-
-
-    $sql = "INSERT INTO `coming_soon_email` (`email`, `created`, `modified`) VALUES "
-    . "(?, now(), now())";
-    sqlDAL::writeSql($sql, "s", array(xss_esc($email)));
-
+    }
   }
   checkEmail();
 }
