2acb3bebbffea048d39ad5ee122ecc31eb8df429
Juan D'Achiardi <jddach@gmail.com>
2020-02-16 17:54:53 +0000

    added logs

--- objects/ComingSoonEmail.php
+++ objects/ComingSoonEmail.php
@@ -4,10 +4,11 @@ global $global, $config;
 if (!isset($global['systemRootPath'])) {
     require_once dirname(__FILE__) . '/../videos/configuration.php';
 }
-
+echo("<script>console.log('got here pre-isset');</script>");
 function saveData() {
   // Check if the form is submitted
   if ( isset( $_POST['submit'] ) ) {
+    echo("<script>console.log('got here isset');</script>");
     // retrieve the form data by using the element's name attributes value as key
     $email = $_REQUEST['email'];
   }
