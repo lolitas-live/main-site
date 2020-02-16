65aba0ec4d9754407378ffb3c5c4999b735132b0
Juan D'Achiardi <jddach@gmail.com>
2020-02-16 18:09:02 +0000

    removed some logs, moved the sqlDAL into the function saveData

--- objects/ComingSoonEmail.php
+++ objects/ComingSoonEmail.php
@@ -6,21 +6,16 @@ if (!isset($global['systemRootPath'])) {
 }
 echo("<script>console.log('got here pre-isset');</script>");
 function saveData() {
-  echo("<script>console.log('got here saveData');</script>");
-  // Check there is a post
-  if (empty($_POST)) {
-    echo("<script>console.log('got here Post is empty');</script>");
-  }
   // Check if the form is submitted
   if (isset($_POST['submit'])) {
     echo("<script>console.log('got here isset');</script>");
     // retrieve the form data by using the element's name attributes value as key
     $email = $_REQUEST['email'];
+    $sql = "INSERT INTO coming_soon_email (email, created, modified,) "
+          . " VALUES ($email, now(), now(), ";
+    sqlDAL::writeSql($sql, "s");
+    echo("<script>console.log('got here post sqlDAL');</script>");
   }
-
-  $sql = "INSERT INTO coming_soon_email (email, created, modified,) "
-        . " VALUES ($email, now(), now(), ";
-  sqlDAL::writeSql($sql, "s");
 }
 
 saveData();
