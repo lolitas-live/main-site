907d0c4639fc3a1debc75200f00a9dd911cb8e7e
Juan D'Achiardi <jddach@gmail.com>
2020-02-16 03:15:01 +0000

    without class

--- objects/ComingSoonEmail.php
+++ objects/ComingSoonEmail.php
@@ -2,20 +2,19 @@
 
 global $global, $config;
 
-class ComingSoonEmail {
 
-  private $email;
+private $email;
 
-  function saveData() {
-    // Check if the form is submitted
-    if ( isset( $_POST['submit'] ) ) {
-      // retrieve the form data by using the element's name attributes value as key
-      $email = $_REQUEST['email'];
-    }
-
-    $sql = "INSERT INTO coming_soon_email (email, created, modified,) "
-          . " VALUES ($email, now(), now(), ";
-    sqlDAL::writeSql($sql);
+function saveData() {
+  // Check if the form is submitted
+  if ( isset( $_POST['submit'] ) ) {
+    // retrieve the form data by using the element's name attributes value as key
+    $email = $_REQUEST['email'];
   }
+
+  $sql = "INSERT INTO coming_soon_email (email, created, modified,) "
+        . " VALUES ($email, now(), now(), ";
+  sqlDAL::writeSql($sql);
 }
+
 saveData();
