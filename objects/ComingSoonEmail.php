318bdb233cc8f647ef27ca261fc5038b74baca74
Juan D'Achiardi <jddach@gmail.com>
2020-02-16 18:12:56 +0000

    add log to check for email value

--- objects/ComingSoonEmail.php
+++ objects/ComingSoonEmail.php
@@ -4,16 +4,17 @@ global $global, $config;
 if (!isset($global['systemRootPath'])) {
     require_once dirname(__FILE__) . '/../videos/configuration.php';
 }
-echo("<script>console.log('got here pre-isset');</script>");
+
 function saveData() {
   // Check if the form is submitted
   if (isset($_POST['submit'])) {
-    echo("<script>console.log('got here isset');</script>");
+
     // retrieve the form data by using the element's name attributes value as key
     $email = $_REQUEST['email'];
+    echo "<script>console.log('Email: " . $email . "' );</script>";
     $sql = "INSERT INTO coming_soon_email (email, created, modified,) "
           . " VALUES ($email, now(), now(), ";
-    sqlDAL::writeSql($sql, "s");
+    sqlDAL::writeSql($sql);
     echo("<script>console.log('got here post sqlDAL');</script>");
   }
 }
